7ff2faee38409125797f2f0a99a0bcf620e5a3a1
Kyle Armstrong <kylearmstrong96@outlook.com>
2020-12-07 21:18:25 +0000

    modified setup and requirement files

--- setup.sh
+++ setup.sh
@@ -2,14 +2,21 @@
 
 apt update
 
+apt install software-properties-common
+apt install build-essential
 apt install python3.8
-apt install pip3
+apt install python3-pip
+apt install python3-dev
 apt install virtualenv
 apt install ffmpeg
+apt install libpcap-dev libpq-dev libffi-dev pkg-config libcairo2-dev
+apt install python3-django
+apt install libsystemd-dev libgirepository1.0-dev libdbus-glib-1-dev libpython3-dev
 
+pip3 install virtualenv
 python3 -m virtualenv musiq_packages
-
 source ./musiq_packages/bin/activate
+pip3 install testresources
 
 pip3 install -r requirements.txt
 
